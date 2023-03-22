4e1889ed0b4592825084e991f73a4a11d2701fe5
Caleb Kemboi <kemboicaleb5@gmail.com>
2023-03-22 12:30:21 +0300

    Add files via upload

---
+++ Qstn 1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Linq;
+using Xunit;
+
+static int[] ToRemoveDuplicates(int[] arr)
+{
+    HashSet<int> set = new HashSet<int>(arr);
+    int[] result = new int[set.Count];
+    set.CopyTo(result);
+    return result;
+
+}
+
+  void TestToRemoveDuplicates()
+{
+
+    int[] arr1 = new int[] {1, 2 , 3, 4, 5, 6, 1};
+    int[] result = ToRemoveDuplicates(arr1);
+    Assert.Equal(new int[] {1, 2, 3, 4, 5, 6}, result);
+    int[] arr2 = new int[] { 1, 1, 1, 1, 1 };
+    int[] result2 = ToRemoveDuplicates(arr2);
+    Assert.Equal(new int[] {1}, result2);
+
+    int[] arr3 = new int[] {};
+    int[] result3 = ToRemoveDuplicates(arr3);
+    Assert.Equal(new int[] { }, result3);  
+
+}
